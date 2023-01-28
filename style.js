74c43a6185141a99100c103a1a724c2971b5a232
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-01-28 14:34:20 +0100

    modification finale

--- style.js
+++ style.js
@@ -1,11 +1,21 @@
 const openModal = document.querySelector(".open-btn");
-const closeModal = document.querySelector(".close-btn");
+const closeButton = document.querySelector(".close-btn");
 const modal = document.querySelector(".modal");
+const modalTransition = [
+    {transform: 'scale(0)'},
+    {transform: 'scale(1)'}
+];
+
+const modalTiming = {
+    duration: 200,
+    iterations: 1,
+};
 
 openModal.addEventListener('click', () => {
     modal.showModal();
+    modal.animate(modalTransition, modalTiming);
 });
 
-closeModal.addEventListener('click', () => {
-    modal.close();
-})
+closeButton.addEventListener('click', () => {
+    modal.close()
+});
